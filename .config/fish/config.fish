d35285f6187f7c855b6caca78ff9a6639611056a
Manas Sambare <manas.sambare@gmail.com>
2024-07-20 23:41:14 +0530

    [Fish] Autostart Zellij Session

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,6 +17,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -Ux PYENV_ROOT $HOME/.pyenv
     set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 
