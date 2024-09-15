272bc0d65fc8b709f13778e4e8de943f7ba31724
Manas Sambare <manas.sambare@gmail.com>
2024-09-15 23:04:37 +0530

    [Polybar] Add VPN module

---
+++ .config/polybar/modules/scripts/get_vpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# All "custom" vpns, including WireGuard, are prefixed with "vpn-" or "wireguard-" as the tunnel name
+VPN_NAME=$(nmcli -t -f NAME,TYPE,STATE con show --active | awk -F: '$2=="vpn" || $2=="wireguard" {print $1}')
+
+if [[ "${VPN_NAME}" != "" ]]; then
+  echo "%{F#5af78e}%{F-} ${VPN_NAME}"
+else
+  echo "%{F#ff5c57}""%{F-}"
+  echo "%{F#5af78e}""%{F-}"
+fi
