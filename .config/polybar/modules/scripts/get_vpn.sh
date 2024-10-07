56ef494043b4e2adf2b8216c2c3199a1bd27db36
Manas Sambare <manas.sambare@gmail.com>
2024-10-07 19:56:59 +0530

    [Polybar] Update VPN Icon and Padding

--- .config/polybar/modules/scripts/get_vpn.sh
+++ .config/polybar/modules/scripts/get_vpn.sh
@@ -4,7 +4,7 @@
 VPN_NAME=$(nmcli -t -f NAME,TYPE,STATE con show --active | awk -F: '$2=="vpn" || $2=="wireguard" {print $1}')
 
 if [[ "${VPN_NAME}" != "" ]]; then
-  echo "%{F#5af78e}""%{F-} ${VPN_NAME}"
+  echo "%{F#5af78e}""%{F-} ${VPN_NAME} "
 else
   echo "%{F#ff5c57}""%{F-}"
   echo "%{F#5af78e}""%{F-}"
