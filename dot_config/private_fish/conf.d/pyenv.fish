377e5c314125144df457b413d358f8cddf104ce6
Manas Sambare <manas.sambare@gmail.com>
2024-11-03 23:25:58 +0530

    [Fish] Remove pyenv virtualenv-init from fish config

--- dot_config/private_fish/conf.d/pyenv.fish
+++ dot_config/private_fish/conf.d/pyenv.fish
@@ -1,10 +1,8 @@
 
 # Enable virtualenv autocomplete
 status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
 
 
 # Enable virtualenv autocomplete
 status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
 
