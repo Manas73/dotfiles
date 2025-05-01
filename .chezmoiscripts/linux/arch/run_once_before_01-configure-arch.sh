3450d7a94e404a39c0567b8e112d3aadb077d630
Manas Sambare <manas.sambare@gmail.com>
2025-05-01 17:33:39 +0530

    [Chezmoi] Decleratively Install Arch Packages

---
+++ .chezmoiscripts/linux/arch/run_once_before_01-configure-arch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+LINE="-------------------------------------------"
+
+export PATH="${HOME}/.local/bin:${PATH}"
+
+if ! command -v pacman &>/dev/null; then
+  echo "This script is only for Arch based systems. Exiting..."
+  echo "${LINE}"
+  exit 1
+fi
+
+echo "Arch based system found!"
+
+echo "${LINE}"
+
+# Install AUR helpers & Flatpak
+sudo pacman -S --noconfirm yay flatpak --needed
+
+echo "${LINE}"
