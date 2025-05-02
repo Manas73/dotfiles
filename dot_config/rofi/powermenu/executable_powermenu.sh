77b770052f37e780e89642fbdf32066413361d7a
Manas Sambare <manas.sambare@gmail.com>
2025-05-02 18:53:42 +0530

    [i3] Update i3 Power Settings

--- dot_config/rofi/powermenu/executable_powermenu.sh
+++ dot_config/rofi/powermenu/executable_powermenu.sh
@@ -66,19 +66,9 @@ run_cmd() {
 		elif [[ $1 == '--reboot' ]]; then
 			systemctl reboot
 		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
+			loginctl lock-session
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
+            pkill -u ${USER}
 		fi
 	else
 		exit 0
