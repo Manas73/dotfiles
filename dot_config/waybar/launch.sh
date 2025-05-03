9656c226e9fb0c433b647a5acb5d96aa5fc58379
Manas Sambare <manas.sambare@gmail.com>
2025-05-03 15:28:25 +0530

    [Hyprland] Add Hyprland Config

---
+++ dot_config/waybar/launch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#                    __
+#  _    _____ ___ __/ /  ___ _____
+# | |/|/ / _ `/ // / _ \/ _ `/ __/
+# |__,__/\_,_/\_, /_.__/\_,_/_/
+#            /___/
+#
+# -----------------------------------------------------
+# Quit all running waybar instances
+# -----------------------------------------------------
+killall waybar
+pkill waybar
+sleep 0.5
+
+# -----------------------------------------------------
+# Default theme: /THEMEFOLDER;/VARIATION
+# -----------------------------------------------------
+themestyle="/ml4w-modern;/ml4w-modern/light"
+IFS=';' read -ra arrThemes <<<"$themestyle"
+echo ":: Theme: ${arrThemes[0]}"
+
+# -----------------------------------------------------
+# Loading the configuration
+# -----------------------------------------------------
+config_file="config"
+style_file="style.css"
+
+# Check if waybar-disabled file exists
+if [ ! -f $HOME/.config/ml4w/settings/waybar-disabled ]; then
+    waybar -c ~/.config/waybar/themes${arrThemes[0]}/$config_file -s ~/.config/waybar/themes${arrThemes[1]}/$style_file &
+else
+    echo ":: Waybar disabled"
+fi
