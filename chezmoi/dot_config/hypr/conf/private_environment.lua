a957eef6a4eeb264c26d28e5251c879d46f61964
Manas Sambare <manas.sambare@gmail.com>
2026-05-22 01:02:16 +0530

    [Hypr] Add HYPRCURSOR_SIZE env var and Vivaldi hardware acceleration fixes
    Set __GL_VRR_ALLOWED=0 and NVD_BACKEND=direct to resolve Vivaldi hardware acceleration issues on NVIDIA under Hyprland.

--- chezmoi/dot_config/hypr/conf/private_environment.lua
+++ chezmoi/dot_config/hypr/conf/private_environment.lua
@@ -4,9 +4,12 @@
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XDG_MENU_PREFIX",           "plasma-")
+hl.env("XDG_MENU_PREFIX","plasma-")
+hl.env("LIBVA_DRIVER_NAME","nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME","nvidia")
+hl.env("XCURSOR_SIZE","24")
+hl.env("HYPRCURSOR_SIZE","24")
 
-hl.env("LIBVA_DRIVER_NAME",         "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("XCURSOR_SIZE",              "24")
-hl.env("HYPRCURSOR_SIZE",           "24")
+-- Vivladi Hardware Acceleration fix
+hl.env("__GL_VRR_ALLOWED" , "0")
+hl.env("NVD_BACKEND", "direct")
