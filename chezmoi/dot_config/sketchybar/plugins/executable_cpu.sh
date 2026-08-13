7208e786f22cbf591ca30783ffd7e4309b6766dd
Manas Sambare <manas.sambare@gmail.com>
2026-08-13 19:10:35 +0530

    [Darwin] Remove Sketchybar

--- chezmoi/dot_config/sketchybar/plugins/executable_cpu.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# CPU usage percentage (100 - idle) from top.
-CPU=$(top -l 1 -n 0 | awk -F'[ %]+' '/CPU usage/ {printf "%.0f", $3 + $5}')
-[ -z "$CPU" ] && CPU=0
-
-sketchybar --set "$NAME" label="${CPU}%"
